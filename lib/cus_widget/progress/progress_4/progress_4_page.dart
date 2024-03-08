95385c6ac869e455e239bc2090d0950ab541843c
lebanc.guo <leblanc.guo@eclicktech.com.cn>
2024-03-08 14:59:33 +0800

    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
    fix

---
+++ lib/cus_widget/progress/progress_4/progress_4_page.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Progress4Page extends StatefulWidget {
+  const Progress4Page({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Progress4PageState();
+
+}
+
+class _Progress4PageState extends State<Progress4Page>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
